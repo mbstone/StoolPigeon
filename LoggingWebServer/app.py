97dfd6cc61b0c5d17d0fcb4f0863469d0f8ef856
Matt Stone <matthew.brossard.stone@gmail.com>
2026-02-22 16:19:29 -0500

    added POST method in logging server

--- LoggingWebServer/app.py
+++ LoggingWebServer/app.py
@@ -1,6 +1,8 @@
 import logging
 
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import Dict, Optional
 from fastapi.responses import PlainTextResponse
 from datetime import datetime
 
@@ -10,7 +12,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG) # Set the minimum level for the console handler
@@ -23,10 +24,20 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+
+class Event(BaseModel):
+    fsr: Optional[int] = None
+    voltage: Optional[float] = None
+    resistance: Optional[float] = None
+    message: Optional[str] = None
+
+
+
+
 logger.info("server starting up")
 
 @app.get("/log")
-async def log_data(request: Request):
+async def log_data_get(request: Request):
     query_params = dict(request.query_params)
     fsr = query_params.get("fsr", "N/A")
     voltage = query_params.get("voltage", "N/A")
@@ -37,5 +48,16 @@ async def log_data(request: Request):
     logger.info(log_entry)
     
     return PlainTextResponse(f"Logged: {log_entry}")
+
+@app.post("/log")
+async def log_data_post(event: Event):
+    fsr = event.fsr
+    voltage = event.voltage
+    resistance = event.resistance
+    message = event.message    
+    log_entry = f"[{datetime.now().isoformat()}] FSR: {fsr}, Voltage: {voltage}, Resistance: {resistance}, Message: {message}"
+    logger.info(log_entry)    
+    return event
+
 
 
